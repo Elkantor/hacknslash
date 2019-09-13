54931e98753b08042d6dabba73f8e68d9978c6f3
Victor GALLET <V_GALLET@domad.local>
2019-09-13 16:51:13 +0200

    Add a module to handle the logging (generates a log.html file)

--- src/modules/logging.c
+++ src/modules/logging.c
@@ -1,32 +1,73 @@
+#include <stdio.h>
+#include <stdarg.h>
+
 #if defined _WIN32 && defined module_logging_build_dll
-    #define LOG __declspec(dllexport) // We are building log as a win32 shared library (.dll)
-    #include <stdio.h>
+    #define LOGGING __declspec(dllexport) // We are building log as a win32 shared library (.dll)
 #elif defined _WIN32 && defined module_logging
-    #define LOG __declspec(dllimport) // we are using log as a win32 shared library (.dll)
+    #define LOGGING __declspec(dllimport) // we are using log as a win32 shared library (.dll)
 #else
-    #define LOG // We are building or using log as a static library (or a shared one on linux)
+    #define LOGGING // We are building or using log as a static library (or a shared one on linux)
 #endif
 
-int log_open(FILE* out_log_file, const char* in_file_name){
-    out_log_file = fopen(in_file_name, "a+"); // open file and append to it's content
-    if(out_log_file == NULL){
-        printf("can't open the log file\n");
+void logging_flush(const char* in_file_name){
+    /*
+        flush the content from the log file by opening it in
+        writing mode, which erased the previous content, and
+        closing it as soon as it's opened
+    */
+    fclose(fopen(in_file_name, "w"));
+}
+
+void logging_close(FILE** out_log_file){
+    fclose(*out_log_file);
+}
+
+int logging_open(FILE** out_log_file, const char* in_file_name){
+    *out_log_file = fopen(in_file_name, "a");
+    if(*out_log_file == NULL){
+        printf("Can't open the logging file\n");
         return -1;
     }
-    fprintf(out_log_file, "[LOG]\n");
-    return 0;
+    return 1;
+}
+
+int logging_write(FILE** out_log_file, const char* in_color, const char* in_text, ...){
+    if(*out_log_file == NULL){
+        printf("Can't write in the logging file\n");
+        return -1;
+    }
+
+    /*
+        vlist variable is used here. To know how it works, you can
+        check these links for more info:
+        - https://stackoverflow.com/questions/15830641/pass-many-arguments-to-a-c-function
+        - https://jameshfisher.com/2016/11/23/c-varargs
+    */
+    va_list args;
+    va_start(args, in_text);
+    va_end(args);
+    fprintf(*out_log_file, "<font color=\"%s\">", in_color);
+    vfprintf(*out_log_file, in_text, args);
+    fprintf(*out_log_file, "</font></br>\n");
+    return 1;
 }
 
 #if !defined module_logging
     const 
 #endif
-    struct LOG logging {
-        int (*const open)(FILE* out_log_file, const char* in_file_name);
+    struct LOGGING logging {
+        void (*const flush)(const char* in_file_name);
+        void (*const close)(FILE** out_log_file);
+        int (*const open)(FILE** out_log_file, const char* in_file_name);
+        int (*const write)(FILE** out_log_file, const char* in_color, const char* in_text, ...);
     }
 #if defined module_logging
     ;
 #else
     logging = {
-        .open = log_open,
+        .flush  = logging_flush,
+        .close  = logging_close,
+        .open   = logging_open,
+        .write  = logging_write,
     };
 #endif
