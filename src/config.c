e6ce0726699fb2a63a8e3a3cbfc49444f2f63bb8
Elkantor <vic03@live.fr>
2019-09-14 12:34:46 +0200

    Add every data and modules for the future hot reload feature settings

--- src/config.c
+++ src/config.c
@@ -1,20 +1,49 @@
-// modules available for hot reload
+/*************************************/
+/*  Modules available for hot reload */
+/*************************************/
 
-#define module_physics      KEY_F1
-// #define module_inputs
-// #define module_inputs_key
-
-// #define module_maths
-// #define module_maths_key
+// #define module_inputs        KEY_F2
+// #define module_maths         KEY_F3
+// #define module_physics       KEY_F1
 
 const char* modules_hot_reload[] = {
-    #if defined module_physics
-        "module_physics",
-    #endif
     #if defined modules_inputs
         "module_inputs",
     #endif
+    #if defined module_logging
+        "module_logging",
+    #endif
+    #if defined module_math
+        "module_math",
+    #endif
+    #if defined module_physics
+        "module_physics",
+    #endif
+    #if defined module_resolution
+        "module_resolution",
+    #endif
 };
 
-// data available for hot reload
-// #define data_colors
+/**********************************/
+/*  Data available for hot reload */
+/**********************************/
+
+// #define data_colors          KEY_F5
+// #define data_inputs
+// #define data_logging
+// #define data_player
+
+const char* data_hot_reload[] = {
+    #if defined data_colors
+        "data_colors",
+    #endif
+    #if defined data_inputs
+        "data_inputs",
+    #endif
+    #if defined data_logging
+        "data_logging"
+    #endif
+    #if defined data_player
+        "data_player"
+    #endif
+};
