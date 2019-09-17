4268c7bdd05cbf54d2d0bd10bfcbeaae0a25181c
Elkantor <vic03@live.fr>
2019-09-17 20:52:14 +0200

    Modify the way the ui_main_menu_symbols will be blend with the symbols mask, and add improvment for the logging system

--- src/data/data_ui_main_menu.c
+++ src/data/data_ui_main_menu.c
@@ -6,9 +6,12 @@
     #define DATA_UI_MAIN_MENU // We are building or using data_ui_main_menu as a static library (or a shared one on linux)
 #endif
 
-unsigned int data_ui_main_menu_textures_id[5];
+unsigned int data_ui_main_menu_textures_id[7];
 unsigned short data_ui_main_menu_loaded = 0;
 unsigned short data_ui_main_menu_current_idx = 0;
+
+unsigned short data_ui_symbols_texture_id = 0;
+unsigned int data_ui_symbols_fbo_id = 0;
 
 #if !defined data_ui_main_menu
     const 
@@ -17,6 +20,8 @@ unsigned short data_ui_main_menu_current_idx = 0;
         unsigned int* ids;
         unsigned short* current_loaded;
         unsigned short* current_idx;
+        unsigned short* symbols_idx_id;
+        unsigned int* symbols_fbo_id;
     }
 #if defined data_ui_main_menu
     ;
@@ -25,5 +30,7 @@ unsigned short data_ui_main_menu_current_idx = 0;
         .ids = data_ui_main_menu_textures_id,
         .current_loaded = &data_ui_main_menu_loaded,
         .current_idx = &data_ui_main_menu_current_idx,
+        .symbols_idx_id = &data_ui_symbols_texture_id,
+        .symbols_fbo_id = &data_ui_symbols_fbo_id,
     };
 #endif
--- src/main.c
+++ src/main.c
@@ -22,27 +22,27 @@ void custom_tracelog(const int in_msg_type, const char* in_text, const va_list i
     logging_open(data_logging.file, data_logging.file_name);
 
     switch (in_msg_type){
-        case LOG_INFO: logging.write(data_logging.file, "#21610B", "<u style='font-weight: bold'>[INFO] : </u>"); break;
-        case LOG_ERROR: logging.write(data_logging.file, "#21610B", "<u style='font-weight: bold'>[ERROR]: </u>"); break;
-        case LOG_WARNING: logging.write(data_logging.file, "#21610B", "<u style='font-weight: bold'>[WARN] : </u>"); break;
-        case LOG_DEBUG: logging.write(data_logging.file, "#21610B", "<u style='font-weight: bold'> [DEBUG]: </u>"); break;
+        case LOG_INFO: logging.write(data_logging.file, "<u style='font-weight: bold;'>[INFO] : </u>"); break;
+        case LOG_ERROR: logging.write(data_logging.file, "<u style='font-weight: bold'>[ERROR]: </u>"); break;
+        case LOG_WARNING: logging.write(data_logging.file, "<u style='font-weight: bold'>[WARN] : </u>"); break;
+        case LOG_DEBUG: logging.write(data_logging.file, "<u style='font-weight: bold'> [DEBUG]: </u>"); break;
         default: break;
     }
-    logging.write(data_logging.file, "darkgrey", in_text, in_args);
+    logging.write(data_logging.file, in_text, in_args);
     logging.close(data_logging.file);
 }
 
 int main(int argc, char** argv){
-    /* INITIALIZE LOGGING SYSTEM */
+    /* INITIALIZE LOGGING SYSTEM (ONLY FOR DEBUGGING)*/
     logging.flush(data_logging.file_name);
     logging.open(data_logging.file, data_logging.file_name);
-    logging.write(data_logging.file, "red", "<h1>[LOG]</h2>");
+    logging.write(data_logging.file, "<h1>[LOG]</h2>\n");
     logging.close(data_logging.file);
     
 
     /* INITIALIZE RAYLIB AND OPENGL CONTEXT */
     InitWindow(0, 0, "hacknslash");
-    SetTraceLogLevel(LOG_INFO);
+    SetTraceLogLevel(LOG_NONE);
     SetTraceLogCallback(custom_tracelog);
     SetTargetFPS(60);
     
@@ -54,28 +54,65 @@ int main(int argc, char** argv){
         Texture2D background_tex = LoadTexture("data/ui/main_menu/background.jpg");
         *(data_ui_main_menu.ids + *data_ui_main_menu.current_loaded) = background_tex.id;
         *data_ui_main_menu.current_loaded += 1;
+        logging.open(data_logging.file, data_logging.file_name);
+        logging.write(data_logging.file, "<strong style='color: purple;' name='texture' id='texture_load_%d'>background_tex loaded : %d</strong>", background_tex.id, background_tex.id);
+        logging.close(data_logging.file);
     }
     {
         Image left_arrow_img = LoadImage("data/ui/main_menu/character_selection/trim_textures/trim_arrow_left_select.png");
         Texture2D left_arrow_tex = LoadTextureFromImage(left_arrow_img);
         *(data_ui_main_menu.ids + *data_ui_main_menu.current_loaded) = left_arrow_tex.id;
         *data_ui_main_menu.current_loaded += 1;
+        logging.open(data_logging.file, data_logging.file_name);
+        logging.write(data_logging.file, "<strong style='color: purple;'>left_arrow_tex loaded : %d</strong>", left_arrow_tex.id);
+        logging.close(data_logging.file);
 
         ImageFlipHorizontal(&left_arrow_img);
         Texture2D right_arrow_tex = LoadTextureFromImage(left_arrow_img);
         *(data_ui_main_menu.ids + *data_ui_main_menu.current_loaded) = right_arrow_tex.id;
         *data_ui_main_menu.current_loaded += 1;
+        logging.open(data_logging.file, data_logging.file_name);
+        logging.write(data_logging.file, "<strong style='color: purple;'>right_arrow_tex loaded : %d</strong>", right_arrow_tex.id);
+        logging.close(data_logging.file);
+        UnloadImage(left_arrow_img);
     }
     {
         Texture2D bottom_bar_tex = LoadTexture("data/ui/main_menu/bottom_bar.png");
         *(data_ui_main_menu.ids + *data_ui_main_menu.current_loaded) = bottom_bar_tex.id;
         *data_ui_main_menu.current_loaded += 1;
+        logging.open(data_logging.file, data_logging.file_name);
+        logging.write(data_logging.file, "<strong style='color: purple;'>bottom_bar_tex loaded : %d</strong>", bottom_bar_tex.id);
+        logging.close(data_logging.file);
     }
     {
         Texture2D play_button_tex = LoadTexture("data/ui/main_menu/character_selection/trim_textures/trim_play_button.png");
         *(data_ui_main_menu.ids + *data_ui_main_menu.current_loaded) = play_button_tex.id;
         *data_ui_main_menu.current_loaded += 1;
+        logging.open(data_logging.file, data_logging.file_name);
+        logging.write(data_logging.file, "<strong style='color: purple;'>play_button_tex loaded : %d</strong>", play_button_tex.id);
+        logging.close(data_logging.file);
     }
+    {
+        Image symbols_right_img = LoadImage("data/ui/main_menu/trim_textures/trim_symbols_right.png");
+        Texture2D symbols_right_tex = LoadTextureFromImage(symbols_right_img);
+        *(data_ui_main_menu.ids + *data_ui_main_menu.current_loaded) = symbols_right_tex.id;
+        *data_ui_main_menu.symbols_idx_id = *data_ui_main_menu.current_loaded;
+        *data_ui_main_menu.current_loaded += 1;
+        logging.open(data_logging.file, data_logging.file_name);
+        logging.write(data_logging.file, "<strong style='color: purple;'>symbols_right_tex loaded : %d</strong>", symbols_right_tex.id);
+        logging.close(data_logging.file);
+
+        RenderTexture2D ui_symbols_render_tex = LoadRenderTexture(ui_symbols_width, ui_symbols_height);
+        *data_ui_main_menu.symbols_fbo_id = ui_symbols_render_tex.id;
+        logging.open(data_logging.file, data_logging.file_name);
+        logging.write(data_logging.file, "<strong style='color: purple;'>ui_symbols_render_tex loaded : %d</strong>", ui_symbols_render_tex.id);
+        logging.close(data_logging.file);
+        UnloadImage(symbols_right_img);
+    }
+    logging.open(data_logging.file, data_logging.file_name);
+    logging.write(data_logging.file, "<h3 style='color: darkgrey;'>number textures loaded : %d</h3>", *data_ui_main_menu.current_loaded);
+    logging.close(data_logging.file);
+    Shader shader = LoadShader(0, "data/shaders/grayscale.fs");
     
     /* INITIALIZE DEFAULT KEYS BINDING */
     data_inputs.keys[data_inputs.key_up]    = KEY_Z;
@@ -84,8 +121,20 @@ int main(int argc, char** argv){
     data_inputs.keys[data_inputs.key_left]  = KEY_Q;
     data_inputs.keys[data_inputs.key_space] = KEY_SPACE;
     
+
+    float current_time = 0;
+
     /* MAIN LOOP */
     while(!WindowShouldClose()){
+        {
+            Texture2D symbols_right = { *(data_ui_main_menu.ids + *data_ui_main_menu.symbols_idx_id), ui_symbols_width, ui_symbols_height, 1, 7 };
+            BeginTextureMode((RenderTexture2D){ *data_ui_main_menu.symbols_fbo_id, symbols_right, {0,0,0,0,0}, 0});
+                // Vector2 position = { 1920 - (symbols_right.width/2), 1080 - (symbols_right.height) };
+                Vector2 position = {0, 0};
+                DrawTextureRec(symbols_right, ui_symbols_rectangle, position, WHITE);
+                DrawTextureRec(symbols_right, ui_symbols_rectangle_mask, position, WHITE);
+            EndTextureMode();
+        }
         BeginDrawing();
             *data_ui_main_menu.current_idx = 0;
             ClearBackground(WHITE);
@@ -151,16 +200,83 @@ int main(int argc, char** argv){
                 }
                 DrawTextEx(test, "PLAY", (Vector2){ position.x + (play_button.width/3)/2 - 2*(48/math.phi), position.y + 25}, 48, 1, WHITE);
             }
+            
+            {
+                Texture2D symbols_right = { *(data_ui_main_menu.ids + *data_ui_main_menu.symbols_idx_id), ui_symbols_width, ui_symbols_height, 1, 7 };
+                RenderTexture2D symbols_render_tex = (RenderTexture2D){ *data_ui_main_menu.symbols_fbo_id, symbols_right, {0,0,0,0,0}, 0};
+                // Texture2D symbols_right = { *(data_ui_main_menu.ids + *data_ui_main_menu.current_idx), ui_symbols_width, ui_symbols_height, 1, 7 };
+                *data_ui_main_menu.current_idx += 1;
+                Vector2 position = { 1920 - (symbols_right.width/2), 1080 - (symbols_right.height) };
+                // BeginShaderMode(shader);
+                DrawTextureRec(symbols_render_tex.texture, ui_symbols_rectangle, position, WHITE);
+                    // DrawTextureRec(symbols_right, ui_symbols_rectangle_mask, position, WHITE);
+                // EndShaderMode();
+            }
+            
             DrawFPS(1800, 40);
         EndDrawing();
+
+        /* TMP TIMER FOR ANIMATIONS */
+        // current_time += GetFrameTime();
+        // logging.write(data_logging.file, "green", "<em style='font-weight: bold'>current_time: %f]</em>", current_time);
+        // if(current_time >= 0.16){
+        //     *data_ui_main_menu.current_symbols_anim += 1;
+        //     if(*data_ui_main_menu.current_symbols_anim > 15){
+        //         *data_ui_main_menu.current_symbols_anim = 0;
+        //     }
+        //     current_time = 0;
+        // }
     }
 
-    /* UNLOAD EVERY UI_MAIN_MENU TEXTURES */
-    for(unsigned short i = 0; i > *data_ui_main_menu.current_loaded; i--){
-        Texture2D texture = (Texture2D){ *(data_ui_main_menu.ids + i), 0, 0, 1, 7 };
-        UnloadTexture(texture);
+    {
+        RenderTexture2D symbols_render_tex = (RenderTexture2D){ *data_ui_main_menu.symbols_fbo_id, {0,0,0,0,0}, {0,0,0,0,0}, 0};
+        logging.open(data_logging.file, data_logging.file_name);
+        logging.write(data_logging.file, "<strong style='color: purple;'>unload render texture : %d</strong>", symbols_render_tex.id);
+        logging.close(data_logging.file);
+        UnloadRenderTexture(symbols_render_tex);
+    }
+
+    // {
+    //     Texture2D symbols_right = (Texture2D){ *(data_ui_main_menu.ids + *data_ui_main_menu.current_loaded - 1), 0, 0, 1, 7 };
+    //     logging.open(data_logging.file, data_logging.file_name);
+    //     logging.write(data_logging.file, "purple", "<strong>unload texture : %d</strong>", symbols_right.id);
+    //     logging.close(data_logging.file);
+    //     UnloadTexture(symbols_right);
+    //     *data_ui_main_menu.current_loaded -= 1;
+    // }
+
+    {
+        /* UNLOAD EVERY UI_MAIN_MENU TEXTURES */
+        for(unsigned short i = *data_ui_main_menu.current_loaded; i > 0; --i){
+            Texture2D texture = (Texture2D){ *(data_ui_main_menu.ids + i - 1), 0, 0, 1, 7 };
+            logging.open(data_logging.file, data_logging.file_name);
+            logging.write(data_logging.file, "<strong style='color: purple;' name='texture_%d'>unload texture : %d</strong>", i-1, texture.id);
+            logging.close(data_logging.file);
+            UnloadTexture(texture);
+        }
     }
     UnloadFont(test);
     CloseWindow();
+
+    logging.open(data_logging.file, data_logging.file_name);
+    logging.write(data_logging.file, "<script>\n\
+        for(let i = 0; i < document.getElementsByName('texture').length; ++i){\n\
+            console.log('elem: ' + i);\n\
+            let texture_elem = document.getElementsByName('texture')[i];\n\
+            let associated_elems = document.getElementsByName('texture_'+i);\n\
+            let previous_color = texture_elem.style.color;\n\
+            texture_elem.addEventListener('mouseover', function(){\n\
+                console.log('overed!!!');\n\
+                for(let i = 0; i < associated_elems.length; ++i){\n\
+                    associated_elems[0].style.color = 'red';\n\
+                }\n\
+            });\n\
+            texture_elem.addEventListener('mouseout', function(){\n\
+                for(let i = 0; i < associated_elems.length; ++i){\n\
+                    associated_elems[0].style.color = previous_color;\n\
+                }\n\
+            });\n\
+        }</script>\n");
+    logging.close(data_logging.file);
     return 0;
 }
