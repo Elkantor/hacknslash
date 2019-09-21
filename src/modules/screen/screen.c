c32898968db8e8d30585ebc8c9de8338c64bd5ff
Elkantor <vic03@live.fr>
2019-09-22 00:45:17 +0200

    Reinit the textures and render textures ids to 0 when unloading a screen

--- src/modules/screen/screen.c
+++ src/modules/screen/screen.c
@@ -5,9 +5,12 @@
     #include <raylib.h>
 #endif
 
+#include "../logging/logging.c"
+
 /************************************* [DATA] *************************************************/
 typedef enum screen_current_id { 
     SCREEN_MAIN_MENU,
+    SCREEN_GAME,
 } screen_current_id;
 
 screen_current_id screen                                    = SCREEN_MAIN_MENU;
@@ -26,7 +29,7 @@ static inline void screen_texture_load(const char* in_path){
     screen_data_textures_ids[screen_data_textures_loaded_count] = texture.id;
     screen_data_textures_loaded_count += 1;
     logging_open(logging_data_file_name);
-    logging_write("<div style='display: flex;'>");
+    logging_write("</br><div style='display: flex;'>");
     logging_write("<div style='flex: 1 1 auto; color: blue;'>texture loaded\t%d</div>\n", texture.id);
     logging_write("<div style='flex: 1 1 auto; color: darkgray;'>%s</div>\n", in_path);
     logging_write("</div>");
@@ -68,6 +71,7 @@ static inline void screen_textures_unload(){
         Texture2D texture = (Texture2D){ screen_data_textures_ids[i-1], 0, 0, 1, 7 };
         logging_write("<em style='color: green;'>texture unloaded\t%d\n", texture.id);
         UnloadTexture(texture);
+         screen_data_textures_ids[i-1] = 0;
     }
     logging_close();
 }
@@ -82,6 +86,7 @@ static inline void screen_render_textures_unload(){
     for(unsigned short i = screen_data_render_textures_loaded_count; i > 0; --i){
         RenderTexture2D render_texture = (RenderTexture2D){ screen_data_render_textures_ids[i-1], {0,0,0,0,0}, {0,0,0,0,0}, 0};
         UnloadRenderTexture(render_texture);
+        screen_data_render_textures_ids[i-1] = 0;
     }
 }
 
