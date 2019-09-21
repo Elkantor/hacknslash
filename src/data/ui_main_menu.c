160a97506538c14a8c90cfdee0c59f719ddcc4dd
Elkantor <vic03@live.fr>
2019-09-21 13:10:01 +0200

    Update the menu with new content (logo, character, glow color)

--- src/data/ui_main_menu.c
+++ src/data/ui_main_menu.c
@@ -14,11 +14,16 @@ _Bool ui_main_menu_loaded = false;
 
 /************************************* [PROCEDURES] *******************************************/
 static inline void ui_main_menu_load(void){
-    screen_texture_load("data/ui/main_menu/background.jpg");
-    screen_texture_load("data/ui/main_menu/character_selection/trim_textures/trim_arrow_left_select.png");
-    screen_texture_load("data/ui/main_menu/bottom_bar.png");
-    screen_texture_load("data/ui/main_menu/character_selection/trim_textures/trim_play_button.png");
-    screen_texture_load("data/ui/main_menu/trim_textures/trim_symbols_right.png");
+    /* 1 */ screen_texture_load("data/ui/main_menu/background.jpg");
+    /* 2 */ screen_texture_load("data/ui/main_menu/logo.png");
+    /* 3 */ screen_texture_load("data/ui/main_menu/character_selection/trim_textures/trim_arrow_left_select.png");
+    /* 4 */ screen_texture_load_flip_horizontal("data/ui/main_menu/character_selection/trim_textures/trim_arrow_left_select.png");
+    /* 5 */ screen_texture_load("data/ui/main_menu/character_selection/character.png");
+    /* 6 */ screen_texture_load("data/ui/main_menu/bottom_bar.png");
+    /* 7 */ screen_texture_load("data/ui/main_menu/character_selection/glow_color.png");
+    /* 8 */ screen_texture_load("data/ui/main_menu/character_selection/trim_textures/trim_play_button.png");
+    /* 9 */ screen_texture_load("data/ui/main_menu/symbols_right.png");
+    /* 10 */ screen_texture_load_flip_horizontal("data/ui/main_menu/symbols_right.png");
 
     screen_render_texture_load(ui_arrow_width, ui_arrow_height);
 
@@ -40,6 +45,13 @@ void ui_main_menu_draw(void){
     }
     {
         int current_texture_id = screen_data_textures_ids[screen_data_textures_current_idx];
+        Texture2D logo = { current_texture_id, ui_logo_width, ui_logo_height, 1, 7 };
+        screen_data_textures_current_idx += 1;
+        Vector2 position = {1920/2 - logo.width/2, 1080/40};
+        DrawTexture(logo, position.x, position.y, WHITE);
+    }
+    {
+        int current_texture_id = screen_data_textures_ids[screen_data_textures_current_idx];
         Texture2D left_arrow = { current_texture_id, ui_arrow_width, ui_arrow_height, 1, 7 };
         screen_data_textures_current_idx += 1;
         Vector2 position = { (1920/40), 500 };
@@ -52,24 +64,40 @@ void ui_main_menu_draw(void){
                 DrawTextureRec(left_arrow, ui_arrow_rectangle_hovered, position, WHITE);
             }
         }
-
-        Vector2 position_right_arrow = { (1920/40)*38, 500 };
-        Rectangle dest = (Rectangle){position_right_arrow.x, position_right_arrow.y - ((left_arrow.height/3) *2), ui_arrow_rectangle.width, ui_arrow_rectangle.height};
-        DrawTexturePro(left_arrow, ui_arrow_rectangle, dest, (Vector2){left_arrow.width, left_arrow.height}, 180, WHITE);
-        Rectangle right_arrow_rec = { position_right_arrow.x, position_right_arrow.y, left_arrow.width, left_arrow.height/3 };
+    }
+    {
+        int current_texture_id = screen_data_textures_ids[screen_data_textures_current_idx];
+        Texture2D right_arrow = { current_texture_id, ui_arrow_width, ui_arrow_height, 1, 7 };
+        screen_data_textures_current_idx += 1;
+        Vector2 position = { (1920/40)*38, 500  };
+        Rectangle right_arrow_rec = { position.x, position.y, right_arrow.width, right_arrow.height/3 };
+        DrawTextureRec(right_arrow, ui_arrow_rectangle, position, WHITE);
         if(CheckCollisionPointRec(GetMousePosition(), right_arrow_rec)){
             if(IsMouseButtonDown(0)){
-                DrawTexturePro(left_arrow, ui_arrow_rectangle_pressed, dest, (Vector2){left_arrow.width, left_arrow.height}, 180, WHITE);
+                DrawTextureRec(right_arrow, ui_arrow_rectangle_pressed, position, WHITE);
             }else{
-                DrawTexturePro(left_arrow, ui_arrow_rectangle_hovered, dest, (Vector2){left_arrow.width, left_arrow.height}, 180, WHITE);
+                DrawTextureRec(right_arrow, ui_arrow_rectangle_hovered, position, WHITE);
             }
         }
+    }
+    {
+        int current_texture_id = screen_data_textures_ids[screen_data_textures_current_idx];
+        Texture2D character = { current_texture_id, ui_character_width, ui_character_height, 1, 7 };
+        screen_data_textures_current_idx += 1;
+        DrawTexture(character, 1920/2 - (ui_character_width/30)*16, 1080 - (ui_character_height/30)*29, WHITE);
     }
     {
         int current_texture_id = screen_data_textures_ids[screen_data_textures_current_idx];
         Texture2D bottom_bar = { current_texture_id, ui_bottom_bar_width, ui_bottom_bar_height, 1, 7 };
         screen_data_textures_current_idx += 1;
         DrawTexture(bottom_bar, 0, 1080 - bottom_bar.height, WHITE);
+    }
+    {
+        int current_texture_id = screen_data_textures_ids[screen_data_textures_current_idx];
+        Texture2D glow_color = { current_texture_id, ui_glow_color_width, ui_glow_color_height, 1, 7 };
+        screen_data_textures_current_idx += 1;
+        Vector2 position = { (1920/2) - (glow_color.width/2), 1080 - 62 - ui_glow_color_height };
+        DrawTextureRec(glow_color, ui_glow_color_rectangle, position, WHITE);
     }
     {
         int current_texture_id = screen_data_textures_ids[screen_data_textures_current_idx];
@@ -93,8 +121,15 @@ void ui_main_menu_draw(void){
         int current_texture_id = screen_data_textures_ids[screen_data_textures_current_idx];
         Texture2D symbols_right = { current_texture_id, ui_symbols_width, ui_symbols_height, 1, 7 };
         screen_data_textures_current_idx += 1;
-        Vector2 position = { 1920 - (symbols_right.width/2), 1080 - (symbols_right.height) };
+        Vector2 position = { 1920 - (symbols_right.width), 1080 - (symbols_right.height) };
         DrawTextureRec(symbols_right, ui_symbols_rectangle, position, WHITE);
+    }
+    {
+        int current_texture_id = screen_data_textures_ids[screen_data_textures_current_idx];
+        Texture2D symbols_left = { current_texture_id, ui_symbols_width, ui_symbols_height, 1, 7 };
+        screen_data_textures_current_idx += 1;
+        Vector2 position = { 0, 1080 - (symbols_left.height) };
+        DrawTextureRec(symbols_left, ui_symbols_rectangle, position, WHITE);
     }
 }
 
