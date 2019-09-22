0ac9f715de2b0a69aafa4c75fa848d3dd3d2f425
Elkantor <vic03@live.fr>
2019-09-22 19:19:41 +0200

    Update the main.c example file for the logging module

--- src/modules/logging/main.c
+++ src/modules/logging/main.c
@@ -3,11 +3,11 @@
 int main(int argc, char** argv){
     // clear the logging file (by default called log.html as the logging_data_file_name variable)
     logging_flush(logging_data_file_name);
-    logging_open(logging_data_file, logging_data_file_name);
+    logging_open(logging_data_file_name);
     
-    logging_write(logging_data_file, "%s %ld", "this is a new stuff", 5165454);
+    logging_write("%s %ld", "this is a new stuff", 5165454);
     const char* html_content = "<em>this is a test in italic mode</em>";
-    logging_write(logging_data_file, "%s", html_content);
+    logging_write("%s", html_content);
     
     logging_close(logging_data_file);
     return 0;
